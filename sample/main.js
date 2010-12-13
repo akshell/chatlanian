a407917b9568802a4e4a9dacd48304d7df6bbc67
korenyushkin <anton@akshell.com>
2010-12-13 10:52:58 +0300

    Placed temporary sample code into main.js.

--- sample/main.js
+++ sample/main.js
@@ -1,22 +1,10 @@
-// The main code file of your application
-
-// Use the basic Akshell library
-require('ak', '0.3').setup();
-
-
-// The index page handler
-var IndexHandler = Handler.subclass(
-  {
-    get: function (request) {
-      return render(
-        'index.html',
-        {
-          header: 'Hello world!'
-        });
-    }
-  });
-
-
-// The URL -> handler mapping
-exports.root = new URLMap(
-  IndexHandler, 'index');
+exports.handle = function (socket) {
+  socket.write(
+    'HTTP/1.1 200 OK\r\n' +
+    'Content-Type: text/plain\r\n' +
+    'Content-Length: 12\r\n' +
+    '\r\n' +
+    'Hello world!');
+  socket.shutdown('send');
+  socket.read();
+};
